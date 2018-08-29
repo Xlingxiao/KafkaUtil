fcc12786d7fccf0253e68e7112863ce1b8ff46fc
Hirefy <994831363@qq.com>
2018-08-29 18:12:56 +0800

    改造http方式的结构逻辑

--- src/main/java/producer/HTTP/workMan.java
+++ src/main/java/producer/HTTP/workMan.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import producer.demo.myProducer;
 
 import java.io.*;
+import java.util.Objects;
 
 /** 
  * @Description:  接收客户端请求使用request进行获取请求内容
@@ -19,6 +20,8 @@ public class workMan implements Runnable {
     private String requestMethod ;
 //    请求地址
     private String requestUrl ;
+//    响应状态码
+    private int statusCode;
 //    响应地址
     private String responseUrl;
 //    请求内容
@@ -32,12 +35,27 @@ public class workMan implements Runnable {
         stringBuilder = new StringBuilder();
     }
 
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
     public void setTopic(String topic) {
         this.topic = topic;
     }
 
-    public void setHttpExchange(HttpExchange httpExchange) {
-        this.httpExchange = httpExchange;
+    public String getResponseUrl() {
+        return responseUrl;
+    }
+
+    public void setResponseUrl() {
+        if (getStatusCode()!=200){
+            responseUrl = "http/Exception/404.html";
+        }else
+            responseUrl = "http/success"+requestUrl;
     }
 
     /**
@@ -48,15 +66,14 @@ public class workMan implements Runnable {
     public void run() {
         try {
             httpRequest(httpExchange);
+            setResponseUrl();
+            httpResponse(httpExchange);
             if(stringBuilder!=null){
-//                httpResponse(httpExchange);
-                sResponse(httpExchange,"SUCCESS");
                 sendToKafka();
-//            使用完StringBuilder之后进行清空否则会导致StringBuilder越来越大
-                stringBuilder.delete(0,stringBuilder.length());
-            }else{
-                sResponse(httpExchange,"FAILD");
             }
+//            使用完StringBuilder之后进行清空否则会导致StringBuilder越来越大
+            stringBuilder.delete(0,stringBuilder.length());
+            httpExchange.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -71,71 +88,59 @@ public class workMan implements Runnable {
 //        获取请求方式、请求URL、设置响应URL
         requestMethod = exchange.getRequestMethod();
         requestUrl = exchange.getRequestURI().getPath();
-        responseUrl = "http/success"+requestUrl;
         System.out.printf("请求方式: %s \t请求路径: %s\n", requestMethod, requestUrl);
+        if (requestMethod.compareToIgnoreCase("post")==0){
+            setStatusCode(200);
 //        将请求内容添加进StringBuilder中
-        BufferedReader br = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
-        String body;
-        while (null != (body = br.readLine())) {
-//            System.out.println(body);
-            stringBuilder.append(body);
+            BufferedReader br = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+            String body;
+            while (null != (body = br.readLine())) {
+                stringBuilder.append(body);
+            }
+            System.out.println(stringBuilder.toString());
+            br.close();
+        }else if(requestMethod.compareToIgnoreCase("get")==0){
+            setStatusCode(404);
         }
-        System.out.println(stringBuilder.toString());
-        br.close();
     }
 
     /**
      * 接收到客户端的请求后对客户端及进行响应
      * @param exchange 交换对象 httpServer包内使用这个对象进行和客户端的交互操作
      */
-    private void sResponse(HttpExchange exchange,String responseContent){
+    private void httpResponse(HttpExchange exchange) {
+//        响应文件
+        File file = null;
+//        响应文件的字节数组长度
+        Long fLength = null;
+//        响应文件的字节内容
+        byte[] fileConnect = null;
+//        响应文件的路径
+        String url;
         try {
-            exchange.sendResponseHeaders(200, responseContent.length());
+            url = Objects.requireNonNull(MyHttpHandler.class.getClassLoader().getResource(getResponseUrl())).getPath();
+            file = new File(url);
+            fLength = file.length();
+            fileConnect = new byte[fLength.intValue()];
+        }catch (Exception e){
+            setStatusCode(404);
+            httpResponse(exchange);
+        }
+        FileInputStream fi;
+        try {
+            assert file != null;
+            fi = new FileInputStream(file);
+            assert fileConnect != null;
+            fi.read(fileConnect);
+            fi.close();
+            exchange.sendResponseHeaders(200, fLength);
             OutputStream os = exchange.getResponseBody();
-            os.write(responseContent.getBytes());
+            os.write(fileConnect);
             os.close();
-            exchange.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-//    private void httpResponse(HttpExchange exchange) {
-////        响应文件
-//        File file = null;
-////        响应文件的字节数组长度
-//        Long fLength = null;
-////        响应文件的字节内容
-//        byte[] fileConnect = null;
-////        响应文件的路径
-//        String url;
-//        try {
-//            url = Objects.requireNonNull(MyHttpHandler.class.getClassLoader().getResource(responseUrl)).getPath();
-//            file = new File(url);
-//            fLength = file.length();
-//            fileConnect = new byte[fLength.intValue()];
-//        }catch (Exception e){
-//            System.out.println("请求地址在上下文中有但是没有响应的html");
-//            responseUrl = "http/Exception/404.html";
-//            httpResponse(exchange);
-//        }
-//        FileInputStream fi;
-//        try {
-////            String requestUrl = exchange.getRequestURI().getPath();
-////            System.out.println(requestUrl);
-//            assert file != null;
-//            fi = new FileInputStream(file);
-//            assert fileConnect != null;
-//            fi.read(fileConnect);
-//            fi.close();
-//            exchange.sendResponseHeaders(200, fLength);
-//            OutputStream os = exchange.getResponseBody();
-//            os.write(fileConnect);
-//            os.close();
-//            exchange.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     private void sendToKafka(){
         if (producer==null)
